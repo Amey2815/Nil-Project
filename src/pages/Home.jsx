11d307ac6e975e2f615adbd09c4f95ef20aa5424
Amey <amey.powar15@gmail.com>
2026-01-28 12:33:20 +0530

    added js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import { assets } from '../assets/assets'
+import { assets } from '../assets/assets.js'
 
 const Home = () => {
 
